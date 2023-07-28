2678f7d1c225164333a78f9acfacce0cf4cabddd
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-07-28 09:21:28 +0530

    commit message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    
+<Navbar title ="Shyam  " about="About us"/> {/* that has the props function that change the title */}
+    </>
+  );
+}
+
+export default App;
