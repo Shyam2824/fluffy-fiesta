34c989e1d146e2c0c13939085177a81190cbb73b
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-08-01 00:14:54 +0530

    textform

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function TextForm(props) {
+    const handdleit= ()=>{
+        console.log("UpperCase Coverted" + text);
+        let newtext= text.toUpperCase();
+        setText(newtext)
+    }
+    const HanddleOnChange= (event)=>{
+        console.log("On change the text");
+        setText(event.target.value)
+    }
+    const [text, setText] = useState('Enter text here');
   return (
         <div>
-            <h1>{props.heading}</h1>
+            <h1>{props.heading}  </h1>
                 <div className="mb-3">
-                    <textarea className="form-control" id="My box" rows="3"></textarea>
+                    <textarea className="form-control" value={text} onChange={HanddleOnChange} id="My box" rows="3"></textarea>
                 </div>
-            <button className="btn btn-primary"> Convert the Upperclass</button>
+            <button className="btn btn-primary" onClick={handdleit}> Convert the Upperclass</button>
         </div>
     )
 }
