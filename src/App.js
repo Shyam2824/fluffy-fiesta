828a2dc85ad9c4628646989318473fd5b478281a
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-07-31 23:11:56 +0530

    textform

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
     
 <Navbar title ="Shyam  " about="About us"/> {/* that has the props function that change the title */}
+<TextForm/>
     </>
   );
 }
---
+++ src/components/TextForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function TextForm() {
+  return (
+    <div>
+       
+<div className="mb-3">
+<label for="My box" className="form-label">Example textarea</label>
+<textarea className="form-control" id="My box" rows="3"></textarea>
+</div>
+    </div>
+  )
+}
