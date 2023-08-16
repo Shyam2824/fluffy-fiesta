ad6b3cd031185d7a8fbbeb5f4f3e8fdaf16deb29
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-08-16 18:18:16 +0530

    Add dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm.1';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
     
 <Navbar title ="Shyam 1 " about="About us"/> {/* that has the props function that change the title */}
-<div className="container">
+<div className="container my-3">
 <TextForm heading=" Enter the text" my-3/>
+<About/>
 </div>
     </>
   );
